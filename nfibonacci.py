5a85ec0d4d2b8daff53296fe8db57c75203cd829
anshikasingh9979-collab <anshikasingh99.79@gmail.com>
2026-08-31 11:59:57 +0530

    Update nfibonacci.py

--- nfibonacci.py
+++ nfibonacci.py
@@ -1,10 +1,15 @@
-n = int(input("Enter your number:"))
-array = [0]*(n+1)
+def fibonacci(n):
+    a, b = 0, 1
+
+    for _ in range(n):
+        a, b = b, a + b
+
+    return a
 
 
-if n >= 1:
-    array[1] = 1
-for i in range(2, n + 1):
-    array[i] = array[i - 1] + array[i - 2]
-print("Your Fibonacci number:", array[n])
-print("Array:", array)
+n = int(input("Enter a number: "))
+
+if n < 0:
+    print("Please enter a non-negative number.")
+else:
+    print("Fibonacci number:", fibonacci(n))
